b0337a4ca70efaea5427eb3034b6e53185a1a1ba
Elsti97 <elsti.shehati@hotmail.it>
2022-11-07 17:13:04 +0100

    Snack 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,77 @@
-// Snack 1
+// ! Snack 1
 // Dato l'array di nomi:
-// const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
 // e dati due numeri min e max (min più piccolo di max).
 // Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
 // Eseguiamo questo esercizio prima con forEach e poi con filter.
 // esempio: dati i valori min: 2 e come max: 4 i nomi nell'array nuovo saranno i nomi compresi tra queste posizioni 2 e 4
 
-
-const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
 let min = 2;
 let max = 4;
 
 // * forEach
-// let arrayCompresi1 = [];
+let arrayCompresi1 = [];
 
-// myArray.forEach((name, numName) => {
-//     if(numName >= min && numName <= max){
-//         arrayCompresi1.push(name);
-//     }
-// })
+myArray.forEach((name, numName) => {
+  if(numName >= min && numName <= max){
+    arrayCompresi1.push(name);
+  }
+})
 
-// console.log(arrayCompresi1);
+console.log(arrayCompresi1);
 
 // * filter
 let arrayCompresi2 = myArray.filter((name, numName) => {
   if( numName >= min && numName <= max){
-      return true
+    return true
   }
 })
 
 console.log(arrayCompresi2);
+
+
+
+// ! Snack 2
+// Dato un elenco degli studenti di una facoltà, con il totale dei loro voti
+let students = [
+    { name: 'Marco', id: 213, grades: 78 },
+    { name: 'Paola', id: 110, grades: 96 },
+    { name: 'Andrea', id: 250, grades: 48 },
+    { name: 'Gaia', id: 145, grades: 74 },
+    { name: 'Luigi', id: 196, grades: 68 },
+    { name: 'Piero', id: 102, grades: 50 },
+    { name: 'Francesca', id: 120, grades: 84 },
+  ];
+// 1- Dobbiamo creare delle targhe con il loro nome in maiuscolo. Ci serve quindi un nuovo array di stringhe. Ogni elemento del nuovo array sarà il nome dello studente ma con tutte le lettere maiuscole.
+// 2-  Dobbiamo creare un nuovo array con gli studenti che hanno un totale di voti superiore a 70
+// 3-  Dobbiamo creare un nuovo array di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+
+// * 1-
+let maiuscolo = students.map((studente) =>{
+  return studente.name.toUpperCase();
+})
+
+console.log(maiuscolo);
+
+// * 2-
+let studenti70 = [];
+
+students.forEach((studente) =>{
+  if(studente.grades > 70){
+    studenti70.push(studente.name);
+  }
+})
+
+console.log(studenti70);
+
+
+// * 3-
+let studenti120 = [];
+
+students.forEach((studente) =>{
+  if(studente.grades > 70 && studente.id > 120){
+    studenti120.push(studente.name);
+  }
+})
+
+console.log(studenti120);
