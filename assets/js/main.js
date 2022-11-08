4287ef0ff193658ab354786e93ccb8bcee75b8fb
Elsti97 <elsti.shehati@hotmail.it>
2022-11-08 19:08:41 +0100

    Snack 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,3 +75,41 @@ students.forEach((studente) =>{
 })
 
 console.log(studenti120);
+
+
+
+// Snack 3
+// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+let limite = 100;
+
+const bici = [
+  {
+    name: 'Alan',
+    peso: 3,
+  },
+  {
+    name: 'Colnago',
+    peso: 5,
+  },
+  {
+    name: 'Daccordi',
+    peso: 9,
+  },{
+  name: 'Blasi',
+  peso: 8,
+  }
+];
+
+
+bici.forEach(({peso}) => {
+  if(peso < limite){
+    limite = peso
+  }
+})
+
+let biciLeggera = bici.find(({peso}) => {
+  return peso == limite;
+})
+
+document.getElementById('biciMinore').innerHTML = `La bici "${biciLeggera.name}" è quella più leggera: ${biciLeggera.peso} kg`
